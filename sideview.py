f3e8990dad58b8a55a2d23a430848b8313082498
JulFrey <julian.frey@iww.uni-freiburg.de>
2023-04-20 10:53:00 +0200

    Added bottom view and wrapped world for image generation

--- sideview.py
+++ sideview.py
@@ -23,6 +23,25 @@ if not os.path.exists(path_out):
 #%%
 
 def points_to_images(path_las, res_im = 256, num_side = 4, plot = False):
+    """
+
+    Parameters
+    ----------
+    path_las : string
+        path to las file
+    res_im : int, optional
+        edge length of the quadratic tensor. The default is 256.
+    num_side : int, optional
+        number of side views. The default is 4.
+    plot : bool, optional
+        Plot the results for debugging. The default is False.
+
+    Returns
+    -------
+    views : TYPE
+        DESCRIPTION.
+
+    """
     
     # read in las file
     las = lp.read(path_las)
@@ -42,7 +61,7 @@ def points_to_images(path_las, res_im = 256, num_side = 4, plot = False):
     # << will be done by augmentation function
     
     # prepare view array
-    views = np.zeros((num_side + 1, res_im, res_im))
+    views = np.zeros((num_side + 2, res_im, res_im))
     
     # add top view
     views[0,:,:] = topview(points, res_im, plot = plot)
@@ -67,11 +86,14 @@ def points_to_images(path_las, res_im = 256, num_side = 4, plot = False):
         # get side images
         views[i+1,:,:] = sideview(points_rot, res_im, plot = plot)
     
+    # add bottom view
+    views[num_side + 1,:,:] = topview(points, res_im, bottom = True, plot = plot)
+    
     # return
     return views
 
 # creating topview
-def topview(points, res_im, plot = False):
+def topview(points, res_im, bottom = False, plot = False):
     
     # find the minimum and maximum values of the x, y, and z coordinates
     x_min, y_min, z_min = np.min(points, axis = 0)
@@ -88,24 +110,43 @@ def topview(points, res_im, plot = False):
     max_axis_x = (x_max - x_min) > (y_max - y_min)
     
     # iterate over each point in the point cloud and update the depth image
-    for point in points:
+    if bottom:
+        for point in points:
+            
+            # calculate the position of the point in the depth image
+            if max_axis_x:
+                x_pos = int((point[0] - x_min) / size)
+                y_pos = int((point[1] - y_med) / size) + int(res_im/2)
+            else:
+                x_pos = int((point[0] - x_med) / size) + int(res_im/2)
+                y_pos = int((point[1] - y_min) / size)
+            
+            # # check if index is out of bounds
+            # if (x_pos > res_im - 1) or (y_pos > res_im -1):
+            #     continue
+            
+            # update the corresponding pixel in the depth image with the z coordinate
+            if (top_image[x_pos % res_im, y_pos % res_im] == -999) | (point[2] < top_image[x_pos % res_im, y_pos % res_im]): # wraped world to avoid out of range indexing
+                top_image[x_pos % res_im, y_pos % res_im] = point[2]
+    else:
+        for point in points:
+            
+            # calculate the position of the point in the depth image
+            if max_axis_x:
+                x_pos = int((point[0] - x_min) / size)
+                y_pos = int((point[1] - y_med) / size) + int(res_im/2)
+            else:
+                x_pos = int((point[0] - x_med) / size) + int(res_im/2)
+                y_pos = int((point[1] - y_min) / size)
+            
+            # # check if index is out of bounds
+            # if (x_pos > res_im - 1) or (y_pos > res_im -1):
+            #     continue
+            
+            # update the corresponding pixel in the depth image with the z coordinate
+            if point[2] > top_image[x_pos % res_im, y_pos % res_im]: # wraped world to avoid out of range indexing
+                top_image[x_pos % res_im, y_pos % res_im] = point[2]      
         
-        # calculate the position of the point in the depth image
-        if max_axis_x:
-            x_pos = int((point[0] - x_min) / size)
-            y_pos = int((point[1] - y_med) / size) + int(res_im/2)
-        else:
-            x_pos = int((point[0] - x_med) / size) + int(res_im/2)
-            y_pos = int((point[1] - y_min) / size)
-        
-        # check if index is out of bounds
-        if (x_pos > res_im - 1) or (y_pos > res_im -1):
-            continue
-        
-        # update the corresponding pixel in the depth image with the z coordinate
-        if point[2] > top_image[x_pos, y_pos]:
-            top_image[x_pos, y_pos] = point[2]
-    
     # replace dummy values with value
     top_image[top_image == -999] = 0
     
@@ -145,13 +186,13 @@ def sideview(points, res_im, plot = False):
             x_pos = int((point[0] - x_med) / size) + int(res_im/2)
             z_pos = int((point[2] - z_min) / size)
         
-        # check if index is out of bounds
-        if x_pos > (res_im - 1) or z_pos > (res_im - 1):
-            continue
+        # # check if index is out of bounds
+        # if x_pos > (res_im - 1) or z_pos > (res_im - 1):
+        #     continue
         
         # update the corresponding pixel in the depth image with the z coordinate
-        if point[1] > side_image[x_pos, z_pos]:
-            side_image[x_pos, z_pos] = point[1]
+        if point[1] > side_image[x_pos % res_im, z_pos % res_im]: # wraped world to avoid out of range indexing
+            side_image[x_pos % res_im, z_pos % res_im] = point[1]
     
     # replace dummy values with value
     side_image[side_image == -999] = 0
@@ -171,4 +212,4 @@ def sideview(points, res_im, plot = False):
 #     points_to_images(path_curr)
 
 # example 
-views = points_to_images(r"D:\Baumartenklassifizierung\data\raw\03498.las", plot = True)
+views = points_to_images(r"D:\TLS\Puliti_Reference_Dataset\train\03492.las", plot = True)
