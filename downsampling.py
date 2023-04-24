f24b2f141c7e5064d98130c974b07e1300dd7d39
Zoe <59600387+zoeschindler@users.noreply.github.com>
2023-04-24 10:07:11 +0200

    change downsampling to pdal

--- downsampling.py
+++ downsampling.py
@@ -8,16 +8,83 @@ Created on Tue Apr 18 08:55:35 2023
 # import packages
 import os
 import glob
-import laspy as lp
-import numpy as np
+# import laspy as lp
+# import numpy as np
+import pdal
 
 # set paths & variables
 path_las = r"S:\3D4EcoTec\train"
-path_out = r"D:\Baumartenklassifizierung\data\train_downsampled"
+path_out = r"D:\Baumartenklassifizierung\data\down"
 
 # create output folder
 if not os.path.exists(path_out):
    os.makedirs(path_out)
+
+#%%
+
+# def downsample(path_las, path_out, res_pc = 0.01, min_n = 100):
+    
+#     """
+#     Parameters
+#     ----------
+#     path_las : str
+#         Path to the input las file.
+#     path_out : str
+#         Path to the output folder.
+#     res_pc : float, optional
+#         Target las resolution. The default is 0.01.
+#     min_n : float, optional
+#         Minimum number of points. The default is 100.
+
+#     Returns
+#     -------
+#     Path to new point cloud.
+#     """
+    
+#     # read in las file
+#     las = lp.read(path_las)
+    
+#     # turn coordinates into numpy array
+#     points = np.stack((las.X, las.Y, las.Z), axis = 1)
+#     points = points * las.header.scale
+    
+#     # check the number of points
+#     if points.shape[0] < min_n:
+#         return ""
+    
+#     # calculate minimum coordinate values
+#     min_coords = np.min(points, axis = 0)
+    
+#     # calculate voxel indices for each point
+#     voxel_indices = np.floor((points - min_coords) / res_pc).astype(int)
+    
+#     # collect non-empty voxels
+#     unique_voxel_indices = np.unique(voxel_indices, axis = 0)
+#     points = (unique_voxel_indices + 0.5) * res_pc + min_coords
+    
+#     # # calculate average point per voxel (takes too long)
+#     # voxel_uniques = np.unique(voxel_indices, axis = 0)
+#     # new_points = np.zeros(voxel_uniques.shape)
+#     # for idx in range(voxel_uniques.shape[0]):
+#     #     new_points[idx,:] = np.mean(points[(voxel_indices == voxel_uniques[idx,:]).all(axis = 1),:], axis = 0)
+#     # points = new_points
+#     # del new_points
+    
+#     # create a new las file
+#     new_header = lp.LasHeader(point_format = 0, version = "1.2")
+#     new_header.offsets = las.header.offset
+#     new_header.scales = las.header.scale
+#     new_las = lp.LasData(new_header)
+#     new_las.x = points[:,0]
+#     new_las.y = points[:,1]
+#     new_las.z = points[:,2]
+    
+#     # write downsampled las
+#     path_out_full = os.path.join(path_out, os.path.basename(path_las))
+#     new_las.write(path_out_full)
+    
+#     # return path
+#     return path_out_full
 
 #%%
 
@@ -34,88 +101,35 @@ def downsample(path_las, path_out, res_pc = 0.01, min_n = 100):
         Target las resolution. The default is 0.01.
     min_n : float, optional
         Minimum number of points. The default is 100.
-
+    
     Returns
     -------
     Path to new point cloud.
     """
+
+    # get output path
+    path_out_full = os.path.join(path_out, os.path.basename(path_las))
     
-    # read in las file
-    las = lp.read(path_las)
+    # setting up downsample
+    las_reader = pdal.Reader(path_las)
+    las_filter = pdal.Filter(type = "filters.voxelcentroidnearestneighbor", cell = res_pc)
+    las_writer = pdal.Writer(path_out_full)
     
-    # turn coordinates into numpy array
-    points = np.stack((las.X, las.Y, las.Z), axis = 1)
-    points = points * las.header.scale
+    # get number of points
+    pipeline = pdal.Pipeline([las_reader])
+    pipeline.execute()
+    n = pipeline.metadata["metadata"]['readers.las']['count']
     
-    # check the number of points
-    if points.shape[0] < min_n:
+    # skip if too few points
+    if n < min_n:
         return ""
     
-    # calculate minimum coordinate values
-    min_coords = np.min(points, axis = 0)
-    
-    # calculate voxel indices for each point
-    voxel_indices = np.floor((points - min_coords) / res_pc).astype(int)
-    
-    # collect non-empty voxels
-    unique_voxel_indices = np.unique(voxel_indices, axis = 0)
-    points = (unique_voxel_indices + 0.5) * res_pc + min_coords
-    
-    # # calculate average point per voxel (takes too long)
-    # voxel_uniques = np.unique(voxel_indices, axis = 0)
-    # new_points = np.zeros(voxel_uniques.shape)
-    # for idx in range(voxel_uniques.shape[0]):
-    #     new_points[idx,:] = np.mean(points[(voxel_indices == voxel_uniques[idx,:]).all(axis = 1),:], axis = 0)
-    # points = new_points
-    # del new_points
-    
-    # create a new las file
-    new_header = lp.LasHeader(point_format = 0, version = "1.2")
-    new_header.offsets = las.header.offset
-    new_header.scales = las.header.scale
-    new_las = lp.LasData(new_header)
-    new_las.x = points[:,0]
-    new_las.y = points[:,1]
-    new_las.z = points[:,2]
-    
-    # write downsampled las
-    path_out_full = os.path.join(path_out, os.path.basename(path_las))
-    new_las.write(path_out_full)
+    # downsample point cloud
+    pipeline = las_reader | las_filter | las_writer
+    pipeline.execute()
     
     # return path
     return path_out_full
-
-#%%
-
-# import pdal
-
-# # Define the PDAL pipeline
-# pipeline = pdal.Pipeline(json.dumps({
-#     "pipeline": [
-#         {
-#             "type": "readers.las",
-#             "filename": "input.las"
-#         },
-#         {
-#             "type": "filters.sample",
-#             "leaf_size": 2.0
-#         },
-#         {
-#             "type": "writers.las",
-#             "filename": "output.las"
-#         }
-#     ]
-# }))
-
-# # Execute the pipeline
-# pipeline.execute()
-
-# # Get the output file path
-# output_file = pipeline.arrays[0]
-
-# # Print some information about the output file
-# print("Downsampled point count:", output_file.shape[0])
-# print("Output file:", output_file.filename)
 
 #%%
 
@@ -123,5 +137,5 @@ def downsample(path_las, path_out, res_pc = 0.01, min_n = 100):
 # for path_curr in glob.glob(os.path.join(path_las, "*.las")):
 #     downsample(path_curr, path_out)
 
-# execution for a single file
-downsample(r"D:\Baumartenklassifizierung\data\raw\03498.las", path_out)
+# # execution for a single file
+# downsample(r"D:\Baumartenklassifizierung\data\12781.las", path_out)
